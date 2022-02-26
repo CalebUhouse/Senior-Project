30cf85ae589d1b632f3c6b4d4c698ca0446b3480
Caleb Uhouse <79284244+CalebUhouse@users.noreply.github.com>
2022-02-25 19:39:50 -0500

    Add files via upload

--- Main.php
+++ Main.php
@@ -1,1 +1,13 @@
+<!DOCTYPE html>
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $Title ?></title>
+    <link href="Website.css" type="text/css" rel="Stylesheet" />
+    <script src="Website.js" defer></script>
+</head>
+<body>
+    <main>
 
+    </main>
+</body>
