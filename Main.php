a3a7b82e438753a1593ef3921aa0d3ab5c539b6d
Caleb Uhouse <79284244+CalebUhouse@users.noreply.github.com>
2022-03-03 12:03:37 -0500

    Add files via upload

--- Main.php
+++ Main.php
@@ -2,12 +2,29 @@
     <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title><?php echo $Title ?></title>
+    <title></title>
     <link href="Website.css" type="text/css" rel="Stylesheet" />
     <script src="Website.js" defer></script>
 </head>
-<body>
+<body class="website-color">
     <main>
-
+        <header class="section-header">
+            <h1>HavenNFTs</h1>
+        </header>
+        <nav>
+            <p class="section header">Home</p>
+            <p class="section header">Men's Sports</p>
+            <p class="section header">Women's Sports</p>
+        </nav>
+        <aside>
+            <p>Aside</p>
+        </aside>
+        <section>
+            <p>Section</p>
+        </section>
+        <footer>
+            <p>Footer</p>
+        </footer>
     </main>
 </body>
+</html>
