8635d161c68c56e948c708a38b8daaae44efec1c
Caleb Uhouse <79284244+CalebUhouse@users.noreply.github.com>
2022-04-28 18:55:11 -0400

    Add files via upload

--- LHU_NFT.sql
+++ LHU_NFT.sql
@@ -1,13 +1,13 @@
 
 
 CREATE TABLE Athlete (
- Athlete_ID int,
- Gender varchar (1),
- Heights int,
- Weight int,
- Sport_ID int,
- Picture VARBINARY(max),
- Postion varchar(5),
+ Athlete_ID int(3),
+ Gender char (1),
+ Ath_Height varchar(5),
+ Ath_Weight int(3),
+ Sport_ID int(1),
+ Picture varchar(max),
+ Position varchar(5),
 
 PRIMARY KEY (Athlete_ID),
 FOREIGN KEY (Sport_ID)
@@ -16,20 +16,20 @@ FOREIGN KEY (Sport_ID)
 
 CREATE TABLE Stats (
 
-Athlete_ID int,
-Sport_ID int,
+Athlete_ID int(3),
+Sport_ID int(1),
 PRIMARY KEY(Athlete_ID,Sport_ID),
 FOREIGN KEY (Athlete_ID,Sport_ID)
 );
 
 CREATE TABLE Basketball (
-Sport_ID int,
-PPG Float(2,1),
+Sport_ID int(1),
+PPG Float(3,1),
 FG Float (3,1),
-Rebounds int,
-Assists int,
-Steals int,
-Blocks int,
+Rebounds int(3),
+Assists int(3),
+Steals int(3),
+Blocks int(3),
 
 
 PRIMARY KEY (Sport_ID)
@@ -37,24 +37,24 @@ PRIMARY KEY (Sport_ID)
 );
 
 CREATE TABLE Football(
-Sport_ID int,
-TDs int,
+Sport_ID int(1),
+TDs int(2),
 Average Yards Float(3,1)
-Tackles int,
-Interceptions int,
-Forced Fumbles int,
+Tackles int(2),
+Interceptions int(2),
+Forced_Fumbles int(2),
 
 PRIMARY KEY (Sport_ID)
 
 );
 
 CREATE TABLE Baseball(
-Sport_ID int,
-Batting Average Float (1,3),
+Sport_ID int(1),
+Batting Average Float (3,3),
 Handedness varchar (1),
-Runs int,
-Hits int,
-RBI int,
+Runs int(3),
+Hits int(3),
+RBI int(3),
 
 PRIMARY KEY (Sport_ID)
 
@@ -63,9 +63,9 @@ PRIMARY KEY (Sport_ID)
 );
 
 CREATE TABLE Soccer(
-Sport_ID int,
-Goals int,
-Assists int,
+Sport_ID int(1),
+Goals int(3),
+Assists int(3),
 
 PRIMARY KEY(Sport_ID)
 
@@ -74,11 +74,11 @@ PRIMARY KEY(Sport_ID)
 
 
 CREATE TABLE Softball (
-Sport_ID int,
-Batting Avge Float (1,3),
-Runs int,
-Hits int,
-RBI int,
+Sport_ID int(1),
+Batting Avge Float (3,3),
+Runs int(3),
+Hits int(3),
+RBI int(3),
 
 PRIMARY KEY (Sport_ID)
 
